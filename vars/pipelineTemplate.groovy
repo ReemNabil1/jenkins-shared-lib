289b1bb1943e9a1993ec5cd2579cdc92929d0fb8
ReemNabil1 <167886829+ReemNabil1@users.noreply.github.com>
2026-05-05 00:28:34 +0300

    Update pipelineTemplate.groovy

--- vars/pipelineTemplate.groovy
+++ vars/pipelineTemplate.groovy
@@ -67,7 +67,7 @@ def call(Map config) {
                 steps {
                     sh "docker stop ${CONTAINER_NAME} || true"
                     sh "docker rm ${CONTAINER_NAME} || true"
-                    sh "docker run -d --name ${CONTAINER_NAME} -p ${PORT}:8080 ${IMAGE_NAME}:${IMAGE_TAG}"
+                    sh "docker run -d --name ${CONTAINER_NAME} -p 8081:8080 ${IMAGE_NAME}:${IMAGE_TAG}"
                 }
             }
         }
